5b079c0214e8bd32bc19b4695fe2f9390ef38bff
atulrwt23 <atulrawat23022003@gmail.com>
2024-11-22 18:39:03 +0530

    Update ramdomPassword.js

--- ramdomPassword.js
+++ ramdomPassword.js
@@ -1,6 +1,7 @@
 const capitalChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const smallChar = "abcdefghijklmnopqrstuvwxyz";
 const specialChar = "#@$&*";
+const number = "1234567890"
 
 function giveNumber(number) {
   return Math.floor(Math.random() * number);
@@ -40,7 +41,7 @@ function isCharMatching(char, string) {
   return false;
 }
 
-function isCarPresent(string, password) {
+function isCharPresent(string, password) {
   for (let index = 0; index < string.length; index++) {
     if (isCharMatching(string[index], password)) {
       return true;
@@ -52,17 +53,23 @@ function isCarPresent(string, password) {
 
 
 function isPasswordValid(password) {
-  return isCarPresent(capitalChar, password);
+  const condition1 = isCharPresent(capitalChar, password);
+  const condition2 = isCharPresent(smallChar, password);
+  const condition3 = isCharPresent(specialChar, password);
+  const condition4 = isCharPresent(number, password);
+
+  return condition1 && condition2 && condition3 && condition4;
+  
 }
 
-function generatePassword(givenLength, passwordLength) {
-  if (passwordLength >= givenLength) {
+function generatePassword(length, passwordLength) {
+  if (passwordLength >= length) {
     return "";
   }
 
   const charToAdd = getDigit();
 
-  return charToAdd + generatePassword(givenLength, passwordLength + 1);
+  return charToAdd + generatePassword(length, passwordLength + 1);
 }
 
 function givePassword(length) {
@@ -80,15 +87,15 @@ function instruction() {
   const segment1 = "\nHELLO USER, SO YOU CAME HERE TO GENERATE A PASSWORD.\n\n";
   const segment2 = "THE PASSWORD MUST CONTAIN AT LEAST 8 DIGITS!!.\n";
   const segment3 = "THE PASSWORD CAN CONTAIN AT MOST 8010 DIGITS!!.\n\n";
-  const segmnet4 = "TELL ME THE NUMBER OF DIGITS IN YOUR PASSWORD : ";
+  const segment4 = "TELL ME THE NUMBER OF DIGITS IN YOUR PASSWORD : ";
 
-  return segment1 + segment2 + segment3 + segmnet4;
+  return segment1 + segment2 + segment3 + segment4;
 }
 
 function passwordGeneration() {
   const length = +prompt(instruction());
 
-  if (length < 8 || length > 8010) {
+  if (length < 4 || length > 8010) {
     console.clear();
     console.log("PLEASE GIVE A VALID LENGTH!!");
     return passwordGeneration();
